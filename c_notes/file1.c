a4727217db96f551302cd5e0d92e5260f045fce2
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-07 12:59:14 +0530

    first commit

---
+++ c_notes/file1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    printf("HELLO WORLD!\n");
+    int a;
+    a = 5;
+    printf("%d",a);
+    // calculating area of a square
+    float side;
+    printf("\nEnter side of a square: ");
+    scanf("%f",&side);
+    printf("area of given side %.2f square area is %.2f", side, side*side);
+        // calculating area of a circle
+    float radius;
+    printf("\nEnter radius of circle: ");
+    scanf("%f",&radius);
+    const float PI = 3.14;
+    printf("area of given radius %.2f circle area is %.2f", radius, PI * radius*radius);
+        return 0;
+}
