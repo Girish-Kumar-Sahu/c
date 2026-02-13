49e19f24d10591ac1001c3da625928bf0f13f603
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-13 17:41:34 +0530

    sample code

--- c_notes/file8.c
+++ c_notes/file8.c
@@ -8,5 +8,13 @@ int main() {
     ptr++;
     printf("%p %c\n", ptr,  *ptr);
 
+    // printing all eng letters
+    for (int i = 65; i < 123; i++)
+    { 
+        if (i>96 || i < 91)
+        {printf("%c & %d ,, ",i, i);}
+    }
+    
+
     return 0;
 }
