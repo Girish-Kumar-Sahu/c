aeb5ebcc009e9d0989a2ab47a7a6c0604a9da595
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-16 21:19:40 +0530

    MORE CODE

--- c_notes/file9.c
+++ c_notes/file9.c
@@ -4,5 +4,20 @@ int main() {
     for (int i = 0; i < 5; i++) {
         printf("marks[%d] = %d\n", i, marks[i]);
     }
+
+    // writing a prgram to enter price of 3 item and return it with gst cost with 18%
+    float items[3], final_val[3];
+    for (int i = 0; i < 3; i++)
+    {
+       printf("enter the price of item no. %d: ", i+1);
+       scanf("%f",&items[i]);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        final_val[i] = 0.18 * items[i] + items[i];
+        printf("%.2f\n", final_val[i]);
+    }
+    
+    
     return 0;
 }
