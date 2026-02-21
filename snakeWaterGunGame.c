ddd4e7fc808d9ec5945cdb0eb12d8385188dfd10
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-21 17:07:10 +0530

    made it functional

--- snakeWaterGunGame.c
+++ snakeWaterGunGame.c
@@ -2,35 +2,39 @@
 #include <stdlib.h>
 #include <time.h>
 
+int checkWinner(int player, int computer) {
+
+    if (player == computer)
+        return 0;
+
+    if ((player == 0 && computer == 1) ||
+        (player == 1 && computer == 2) ||
+        (player == 2 && computer == 0))
+        return 1;
+
+    return -1;
+}
+
 int main() {
 
-    int player;
-    int computer;
+    int player, computer, result;
 
-    // Seed random number
     srand(time(NULL));
-
-    // Generate computer choice (0–2)
     computer = rand() % 3;
 
     printf("Enter 0 for Snake, 1 for Water, 2 for Gun: ");
     scanf("%d", &player);
 
+    result = checkWinner(player, computer);
+
     printf("Computer chose: %d\n", computer);
 
-    if (player == computer) {
-    printf("It's a Draw!\n");
-}
-else if (
-    (player == 0 && computer == 1) ||
-    (player == 1 && computer == 2) ||
-    (player == 2 && computer == 0)
-) {
-    printf("You Win!\n");
-}
-else {
-    printf("Computer Wins!\n");
-}
+    if (result == 0)
+        printf("Draw!\n");
+    else if (result == 1)
+        printf("You Win!\n");
+    else
+        printf("Computer Wins!\n");
 
     return 0;
 }
