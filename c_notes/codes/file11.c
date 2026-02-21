e9746300f752c14939416e56c9d7f945d365413c
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-21 17:01:29 +0530

    string codes

--- c_notes/codes/file11.c
+++ c_notes/codes/file11.c
@@ -1,9 +1,23 @@
-#include <stdio.h>   
+#include <stdio.h>
+#include <string.h>
 
-
-//strings
 int main() {
-   char name[] = "Girish";
-   printf("%s\n",name);
-    return 0;  
+    char name[100];  // allocate writable buffer
+
+    // print a predefined name
+    strcpy(name, "Girish");
+    printf("%s\n", name);
+
+    strcpy(name, "123456");
+    puts(name);
+
+    // let user enter their name and print its length
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+
+    // remove newline if present
+    name[strcspn(name, "\n")] = '\0';
+
+    printf("Your name's length is = %zu\n", strlen(name));
+    return 0;
 }
