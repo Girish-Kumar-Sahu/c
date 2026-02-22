88a369b67d143af9db512f5775f6eedcb52fec5a
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-22 11:55:48 +0530

    custom strlen function

--- c_notes/codes/file11.c
+++ c_notes/codes/file11.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int strLen(char str[]);
+
 int main() {
     char name[100];  // allocate writable buffer
 
@@ -19,5 +21,19 @@ int main() {
     name[strcspn(name, "\n")] = '\0';
 
     printf("Your name's length is = %zu\n", strlen(name));
+    printf("Your name's length is = %zu\n", strLen(name));
+    printf("Your name's address is = %zu\n", name); //printing address
     return 0;
+}
+
+
+//counting lentgh of a string function
+int strLen(char str[]){
+    int count = 0;
+    for (int i = 0; str[i]!='\0' ; i++)
+    {
+        count++;
+    }
+    return count;
+    
 }
