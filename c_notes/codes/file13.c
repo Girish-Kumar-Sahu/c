e963516b484091a381772ec33a55f881032deb43
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-25 22:44:24 +0530

    dice code added

--- c_notes/codes/file13.c
+++ c_notes/codes/file13.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-    printf("%d\n", rand());
-    printf("%d\n", rand());
+
+    srand(time(NULL));
+
+    for(int i=0;i<5;i++){
+        int dice = rand()%6 + 1;
+        printf("Dice: %d\n", dice);
+    }
+
+    return 0;
 }
